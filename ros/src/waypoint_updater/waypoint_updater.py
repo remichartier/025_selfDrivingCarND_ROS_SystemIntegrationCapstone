318915c37870c3bdb6f0c8cf4015d0f0df8a3e45
remichartier <remipr.chartier@gmail.com>
2021-03-07 02:45:13 +0000

    add comments from project lesson

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -2,13 +2,73 @@
 
 '''
 Notes from project lesson : 
+===========================
 
 This package contains the waypoint updater node: waypoint_updater.py. The purpose of this node is to update the target velocity property of each waypoint based on traffic light and obstacle detection data. This node will subscribe to the /base_waypoints, /current_pose, /obstacle_waypoint, and /traffic_waypoint topics, and publish a list of waypoints ahead of the car with target velocities to the /final_waypoints topic.
 
 Suggested Order of Project Development
+=======================================
 Because you will be writing code across several packages with some nodes depending on messages published by other nodes, we suggest completing the project in the following order:
 
 1. Waypoint Updater Node (Partial): Complete a partial waypoint updater which subscribes to /base_waypoints and /current_pose and publishes to /final_waypoints.
+
+The /base_waypoints topic publishes a list of all waypoints for the track, so this list includes waypoints both before and after the vehicle (note that the publisher for /base_waypoints publishes only once). For this step in the project, the list published to /final_waypoints should include just a fixed number of waypoints currently ahead of the vehicle:
+
+The first waypoint in the list published to /final_waypoints should be the first waypoint that is currently ahead of the car.
+The total number of waypoints ahead of the vehicle that should be included in the /final_waypoints list is provided by the LOOKAHEAD_WPS variable in waypoint_updater.py.
+
+We can see that both the /final_waypoints and /base_waypoints topics have message type Lane. You can look at the details about this message type in <path_to_project_repo>/ros/src/styx_msgs/msg/.
+
+message type being used in /final_waypoints:
+
+Type: styx_msgs/Lane
+
+provides the following message information:
+
+std_msgs/Header header
+  uint32 seq
+  time stamp
+  string frame_id
+styx_msgs/Waypoint[] waypoints
+  geometry_msgs/PoseStamped pose
+    std_msgs/Header header
+      uint32 seq
+      time stamp
+      string frame_id
+    geometry_msgs/Pose pose
+      geometry_msgs/Point position
+        float64 x
+        float64 y
+        float64 z
+      geometry_msgs/Quaternion orientation
+        float64 x
+        float64 y
+        float64 z
+        float64 w
+  geometry_msgs/TwistStamped twist
+    std_msgs/Header header
+      uint32 seq
+      time stamp
+      string frame_id
+    geometry_msgs/Twist twist
+      geometry_msgs/Vector3 linear
+        float64 x
+        float64 y
+        float64 z
+      geometry_msgs/Vector3 angular
+        float64 x
+        float64 y
+        float64 z
+
+From here you can see that the messages contain a header and a Waypoint list named waypoints. Each waypoint has pose and twist data. Going further, you can see that twist.twist data contains 3D linear and angular velocities.
+
+Lane message example
+=====================
+As a use-case example, given a single styx_msgs/Lane message my_lane_msg, you can access the x direction linear velocity of the first waypoint in Python with:
+
+my_lane_msg[0].twist.twist.linear.x
+Note that the coordinates for linear velocity are vehicle-centered, so only the x-direction linear velocity should be nonzero.
+
 4. Waypoint Updater (Full): Use /traffic_waypoint to change the waypoint target velocities before publishing to /final_waypoints. Your car should now stop at red traffic lights and move when they are green.
 
 '''
