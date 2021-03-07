7a265361aca9c71fbfad6a1c25fb524386c8cc45
remichartier <remipr.chartier@gmail.com>
2021-03-07 07:49:04 +0000

    1st step : comple partial waypoint updater, publish /final_waypoints

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -78,6 +78,8 @@ from styx_msgs.msg import Lane, Waypoint
 
 import math
 
+from scipy.spatial import KDTree
+
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
 
@@ -107,18 +109,101 @@ class WaypointUpdater(object):
 
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
-
+        
         # TODO: Add other member variables you need below
-
-        rospy.spin()
-
+        self.pose = none
+        self.base_waypoints = none
+        self.waypoints_2d = none
+        self.waypoints_tree = none
+        
+        # rospy.spin()
+        self.loop() ''' We've defined a loop function, because this gives us control over
+        the publishing frequency. So we want to target 50 Hz. You could probably get away
+        with something little less that 50Hz in this case. The final_waypoints we will be 
+        publishing go to the waypoints_follower onto the DBW, which is a piece of code from Autoware, 
+        and we believe it is running at 30 Hz.
+        So we've got that loop that's constantly happening at 50Hz.
+        '''    
+    
+    def loop(self):
+        rate = rospy.rate(50)
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:
+                '''if we have self.pose and self.base_waypoints,
+                we need to get that closest waypoint. We use this function
+                self.get_closest_waypoint_idx() and then we're going to be using
+                that KDTree
+                '''
+                # Get closest waypoint
+                closest_waypoint_idx = self.get_closest_waypoint_idx()
+                self.publish_waypoints(closest_waypoint_idx)
+            rate.sleep()
+    
+    def get_closest_waypoint_idx(self):
+        # get coordinates of the car
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        ''' Then doing query on the tree base on x and y, ',1' for returning only 1 item
+        so the closest point in our KDTree to our query item, the query will return the position
+        and also the index, and the index is in the same order as you put in the KDTree'''
+        closest_idx = self.waypoint_tree.query([x,y],1)[1]
+        
+        # Check if the closest coordinate is ahead or behind vehicle
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx -1]
+        
+        # equation for hyperplane through closes coords
+        ''' We use this hyperplane and then do a dot product to see if that's 
+        positive or negative, and based on that dot product between those 2 vectors,
+        we see if that first wave point is indeed in front of vehicle or not'''
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x,y])
+        
+        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+        
+        '''If it's behind the car, then we just ignore it and cut it off.
+        And we just take the next one'''
+        
+        if val > 0 : # means the closest waypoint is behind us
+            closest_idx = (closest_idx +1) % len(self.waypoints_2d)
+            
+        return closest_idx
+    
+    def publish_waypoints(self, closest_idx):
+        '''Message type needs to be a lane so we create a new lane object, or new
+        lane message, lane header same as base_waypoints header, it does not really matter,
+        we're not going to use the header ... and then fill the waypoints for that lane, starting
+        by the closes_idx to the next LOOKAHEAD_WPS waypoints '''
+        lane = lane()
+        lane.header = self.base_waypoints.header
+        '''Do we have to worry about doing a modular in case it goes over the length of waypoints ?
+        we don't because Python slicing is really nice, so if closest index + LOOKAHEAD waypoints
+        is bigger than the total length of waypoints, it will just give us the slice from the closest
+        to the end of the waypoints'''
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane)
+        
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        # Just store the car's position
+        self.pose = msg # This is called frequently, around 50 Hz
+        
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        pass
+        self.base_waypoints = waypoints ''' store coming waypoints in the object. Latched subscriber, 
+         so once the call back is called, it does not send the base_waypoints anymore.
+         This is good and ok because the base_waypoints are never changing.
+         Basic idea : want to take a chunk of theses waypoints, and use the first 200 that are
+         in the front of the car as a reference.
+        '''
+        # Use KDTree data structure to search the closest point in space really efficiently
+        if not self.waypoints_2d: ''' Because we want to make sure that self.waypoints_2d is initialized before 
+             the subscriber is otherwise could run into risky conditions where the subscriber callback is called 
+             before self.waypoints_2d is initialized, otherwise it would not know what to reference'''
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
